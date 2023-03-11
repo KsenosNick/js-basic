c937ef1e6839916843c0bb40283e3bcb034c28cf
KsenosNick <xenos-nick@mail.ru>
2023-03-11 18:20:20 +0300

    Конвертация типов

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const projectName = 'Сайт магазина';
-const price = 2000;
-const author = 'Василий Пупкин';
+const age = '18';
+console.log(age + 5);
+console.log(Number(age) + 5);
+console.log(age - 5);
+console.log(age / 5);
+console.log(age * 5);
 
-const template = author + ' заказал ' + projectName + ' по цене ' + price;
-const template2 = `${author} \nзаказал ${projectName} \nпо цене ${price}`;
+const userName = 'Вася';
+console.log(Number(userName) + 5);
+console.log(typeof NaN);
 
-console.log(template);
-console.log(template2);
+console.log(String(4) + 7);
+console.log(Boolean(123));
+console.log(Boolean(0));
+console.log(Boolean('123'));
+console.log(Boolean(''));
+console.log(Boolean('' + 2));
+console.log(true + 2);
