a19f7d24cc7720b23ea67655fba6304d0fd42e1c
KsenosNick <xenos-nick@mail.ru>
2023-03-11 10:56:15 +0300

    Порядок операторов

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-// Операторы присваивания
-let age = 18 + 5;
-age += 2; //age = age + 2
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
 
-age -= 3;
-age *= 2;
-age /= 3;
+// 100 - (12) 10 > (10) 90 - (12) 5
 
-age++; //age = age + 1
-age--; //age = age - 1
+const a = (5 + 10) / 2;
+console.log(a);
 
-console.log(age);
+let b;
+let c;
+c = b = 100 + 50;
 
-// Операторы сравнения
-const vasia = 20;
-console.log(age > vasia);
-console.log(age >= vasia);
-console.log(age < vasia);
-console.log(age <= vasia);
-console.log(age == vasia);
+console.log(c);
+console.log(b);
