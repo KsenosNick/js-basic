8450d45173fca32c1fac40f996e2bc0eca239599
KsenosNick <xenos-nick@mail.ru>
2023-03-11 12:49:39 +0300

    Шаблонные строки

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 'use strict';
-/*
-Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
 
-К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
 
-Выведете в консоль:
+const template = author + ' заказал ' + projectName + ' по цене ' + price;
+const template2 = `${author} \nзаказал ${projectName} \nпо цене ${price}`;
 
-Boolean переменную успеете ли вы взяться за работу
-Сколько вы за неё попросите?
-*/
-
-const rate = 80;
-const workingDay = 5;
-const workingWeek = 5;
-const leadTime = 40;
-const daysBeforeDeparture = 11;
-
-console.log('Смогу ли выполнить работу? ' + ((daysBeforeDeparture - 2) * workingWeek > leadTime));
-console.log('Плата за заказ: ' + rate * leadTime);
+console.log(template);
+console.log(template2);
