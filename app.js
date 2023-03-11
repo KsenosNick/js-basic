b13d312c153f7387f21dffbe5a31467fa35d5db4
KsenosNick <xenos-nick@mail.ru>
2023-03-11 18:22:33 +0300

    False из других типов

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 'use strict';
 
-const age = '18';
-console.log(age + 5);
-console.log(Number(age) + 5);
-console.log(age - 5);
-console.log(age / 5);
-console.log(age * 5);
-
-const userName = 'Вася';
-console.log(Number(userName) + 5);
-console.log(typeof NaN);
-
-console.log(String(4) + 7);
-console.log(Boolean(123));
 console.log(Boolean(0));
-console.log(Boolean('123'));
 console.log(Boolean(''));
-console.log(Boolean('' + 2));
-console.log(true + 2);
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sx')));
