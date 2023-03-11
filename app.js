168cfbcddec7f42315bea4b73570a0661bc412b3
KsenosNick <xenos-nick@mail.ru>
2023-03-11 10:43:22 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-let a = 1;
-console.log(a);
+// Базовые арифметические операторы
 
-a = 'test';
-console.log(a);
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3;
 
-const b = 2;
-console.log(b);
+console.log(space);
 
-b = 'test';
+// Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ' ' + street + ' ' + 5);
