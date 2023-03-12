8c9f71c0a3754edf6799c284af6f55a8903d0846
KsenosNick <xenos-nick@mail.ru>
2023-03-12 11:07:26 +0300

    Упражнение - Размещение депозита

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const money = 100;
-const canBuy = money > 50;
+const depositAmount = 12000;
+const depositRate = 0.07;
+const depositeTerm = 2;
+const purchasePrice = 13500;
 
-if (canBuy) {
-  console.log('Может купить наш продукт');
-} else if (money > 5) {
-  console.log('Куплен mini продукт');
+const result = depositAmount * (1 + depositRate / 12) ** (depositeTerm * 12);
+
+if (result > purchasePrice) {
+  console.log(`Всего накоплено ${result} $. Покупка возможна. Остаток на счёте - ${result - purchasePrice} $`);
 } else {
-  console.log('Не хватает денег:-(');
+  console.log('Денег недостаточно');
 }
-
-console.log('Итог');
