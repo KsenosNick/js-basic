339d28c4c8e6771edb2dbd630677f521f7ddcc58
KsenosNick <xenos-nick@mail.ru>
2023-03-13 18:48:14 +0300

    Оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-console.log('Вася' || 'Олег');
-console.log(false || 'Олег');
-
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-
-let a = 'Марина';
-const userName = a || 'Петя';
-console.log(userName);
-
-const isAdmin = true;
-const fileName = isAdmin && 'file.name';
-console.log(fileName);
+const userAge = 'Вася';
+console.log(userAge || 'Default UserAge');
+console.log(userAge && 'Default UserAge');
