4f1e890ed353e5cd9580722282207726de9969e2
KsenosNick <xenos-nick@mail.ru>
2023-03-13 18:44:50 +0300

    Операторы с другими типами

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 'use strict';
 
+console.log('Вася' || 'Олег');
+console.log(false || 'Олег');
+
+console.log('Вася' && 'Олег');
+console.log(false && 'Олег');
+
+let a = 'Марина';
+const userName = a || 'Петя';
+console.log(userName);
+
 const isAdmin = true;
-const canWrite = true;
-const isSuperAdmin = true;
-
-console.log(`Системный файл ${isAdmin && canWrite}`);
-console.log(`Обычный файл ${isAdmin || canWrite}`);
-console.log(`Инвертировать права админа ${!isAdmin}`);
-
-const isEdited = true;
-console.log(
-  `Системный файл с редактированием ${isAdmin && canWrite && !isEdited}`
-);
-console.log(`Системный файл с редактированием ${isAdmin && canWrite && (!isEdited || isSuperAdmin)}`);
-
-if (a === -8 || a === 22) {
-  
-}
+const fileName = isAdmin && 'file.name';
+console.log(fileName);
