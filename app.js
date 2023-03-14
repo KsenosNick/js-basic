a6fa8120a0a04c6cde7d3d5d808207556691c7df
KsenosNick <xenos-nick@mail.ru>
2023-03-14 20:14:19 +0300

    Функции в функциях

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict';
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
 
-function caAccessWebsite(age) {
-  if (age < 18) {
-    return false;
-  }
-  return true;
+function calcWeight(present) {
+  return present * KG_IN_USD;
 }
-console.log(caAccessWebsite(19));
 
-const canAccessWebSite2 = age => age < 18 ? 'Нет' : 'Да';
-console.log(canAccessWebSite2(19));
+function calcDist(distance) {
+  return distance * KM_IN_USD;
+}
+
+function getExchangePrice(present1, present2, distance) {
+  const price1 = calcWeight(present1);
+  const price2 = calcWeight(present2);
+  const distancePrice = calcDist(distance);
+  return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1, 2, 10));
