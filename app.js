705d6015e744995d8d868c3bc83576c72a143ac5
KsenosNick <xenos-nick@mail.ru>
2023-03-14 19:45:01 +0300

    Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 'use strict';
 
-const toPower = (num, power) => num ** power;
+function toPower(num, power = 2) {  
+  const res = num ** power;
+  return res;
+}
+
 console.log(toPower(2, 3));
+console.log(toPower(2));
