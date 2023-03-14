59366d49ad0ee6090563187060152538c5769e23
KsenosNick <xenos-nick@mail.ru>
2023-03-14 18:51:17 +0300

    Анонимные функции

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 'use strict';
 
-function logName(name, surname) {
-  console.log(`Моё имя ${name} ${surname}`);
+function powerOfTwo(num) {
+  return num * num;
 }
 
-logName('Николай', 'Малютин');
+console.log(powerOfTwo(5));
 
-function countDepositeSum(depositInUSD, month, rate) {
-  return depositInUSD * (1 + rate / 12) ** month;
-}
+const poft = function (num) {
+  return num * num;
+};
 
-const example1 = countDepositeSum(1000, 24, 0.12);
-console.log(example1);
-
-const example2 = countDepositeSum(1000, 48, 0.1);
-console.log(example2);
+console.log(poft(6));
