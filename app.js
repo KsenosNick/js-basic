bdf454cd8cc35dbd02f453d72523e0f42b4c746d
KsenosNick <xenos-nick@mail.ru>
2023-03-16 19:31:02 +0300

    Slice, splice, concat, reverse

--- app.js
+++ app.js
@@ -1,20 +1,2 @@
 'use strict';
 
-const roles = ['user', 'admin', 'manager'];
-
-const elIndex1 = roles.indexOf('admin');
-console.log(elIndex1);
-
-const elIndex2 = roles.indexOf('superuser');
-console.log(elIndex2);
-
-if (roles.indexOf('admin') >= 0) {
-  console.log('Доступ есть');
-}
-
-console.log(roles.includes('admin'));
-console.log(roles.includes('superadmin'));
-
-if (roles.includes('admin')) {
-  console.log('Доступ есть');
-}
