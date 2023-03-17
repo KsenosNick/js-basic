58fa2f72c883e42982a292b4ec3b3ee5c9cbe5fd
KsenosNick <xenos-nick@mail.ru>
2023-03-17 19:10:22 +0300

    Rest оператор

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const userData = ['Николая', 38, 'Выкса'];
-
-function getData() {
-  return ['Николая', 38, 'Выкса'];
-}
-
-// const userName = [0];
-// const age = getData()[1];
-// const city = getData()[2];
-
-const [userName, _, city] = getData();
-
-console.log(userName, city);
+const data = [1, 2, 3, 4, 5, 6, 7, 8];
+const [one, two, ...others] = data;
+console.log(one, two, ...others);
