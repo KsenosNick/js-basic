a3b194b5fbcd2efce1e39a368fbaa50f716972f6
KsenosNick <xenos-nick@mail.ru>
2023-03-18 16:17:12 +0300

    Цикл for

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const url = 'https://purpleschool.ru/course/javascript';
-
-function urlSplit(url) {
-  let [protocol, _, domenName, ...path] = url.split('/');
-  protocol = protocol.split(':')[0];
-  path = '/' + path.join('/');
-
-  console.log(
-    `Протокол: ${protocol}, доменное имя: ${domenName}, путь внутри сайта: ${path}`
-  );
+for(let i = 1; i < 10; i++) {
+    console.log(`Наш баланс ${i}$`);
 }
-
-urlSplit(url);
