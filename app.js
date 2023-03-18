fbb50c705ad0a9da25f187f1404750e8f64a90ed
KsenosNick <xenos-nick@mail.ru>
2023-03-18 17:05:47 +0300

    Циклы for of и for in

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
 'use strict';
-
-for (let i = 1; i < 5; i++) {
-  console.log(`Цикл 1 - ${i}`);
-  for (let j = 1; j < 5; j++) {
-    console.log(`Цикл 2 - ${i}`);
-  }
-}
-
-let i = 1;
-
-while (i < 5) {
-  console.log(`Вывод ${i}`);
-  i++;
-}
-
 const arr = [1, 4, 8, 7];
 
-for (let index = 0; index < arr.length; index++) {
-  if (arr[index] > 5) {
-    break;
-  }
+for (let element of arr) {
+  console.log(element);
+}
+
+for (let index in arr) {
   console.log(arr[index]);
 }
-
-i = 0;
-
-while (arr[i] <= 5 && i < arr.length) {
-  console.log(arr[i]);
-  i++;
-}
-
-let j = 0;
-
-do {
-  console.log(j);
-  j++;
-} while (j < 0);
