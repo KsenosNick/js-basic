4d72f7a10b3cca093077a3b909ad2b6f361d2b40
KsenosNick <xenos-nick@mail.ru>
2023-03-19 18:52:46 +0300

    Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 'use strict';
 
-function power(pow) {
-  return function (num) {
-    return num ** pow;
-  };
-}
-
-const powerOfTwo = power(2);
-console.log(powerOfTwo(5));
-console.log(powerOfTwo(10));
-
-const powerOfThree = power(3);
-console.log(powerOfThree(5));
-console.log(powerOfThree(10));
+const power = (pow) => (num) => num ** pow;
 
 console.log(power(5)(4));
