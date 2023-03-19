92568a8d96d8cfab417f44348ce648efdf12e02c
KsenosNick <xenos-nick@mail.ru>
2023-03-19 12:49:05 +0300

    Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 'use strict';
-const arr = [1, 4, 8, 7];
 
-for (let element of arr) {
-  console.log(element);
+const startBalance = 100;
+const operations = [1000, -700, 300, -500, 10000];
+
+function getBalance(startBalance, operations) {
+  let balance = startBalance;
+  for (const i of operations) {
+    balance += i;
+  }
+  return balance;
 }
 
-for (let index in arr) {
-  console.log(arr[index]);
+function checkBalance(startBalance, operations) {
+  let balance = startBalance;
+  for (const i of operations) {
+    if (balance < 0) {
+      return false;
+    }
+    balance += i;
+  }
+  return true;
+}
+
+function averageOperations(operations) {
+  let incomeSum = 0;
+  let incomeCount = 0;
+  let consumptionSum = 0;
+  let consumptionCount = 0;
+
+  for (const i of operations) {
+    if (i > 0) {
+      incomeSum += i;
+      incomeCount++;
+    } else {
+      consumptionSum += i;
+      consumptionCount++;
+    }
+  }
+
+  return [incomeSum / incomeCount, consumptionSum / consumptionCount];
 }
