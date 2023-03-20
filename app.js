6c30c5d81462a1c342a0588ce19d7473d305b406
KsenosNick <xenos-nick@mail.ru>
2023-03-20 18:44:04 +0300

    forEach addition

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const score = [5, 10, 0, 15];
 }
  */
 
-const iterator = (el, i) => {
-  console.log(`Раунд ${i + 1}: ${el}`);
+const iterator = (scroreEl, i) => {
+  console.log(`Раунд ${i + 1}: ${scroreEl}`);
 };
 
 score.forEach(iterator);
