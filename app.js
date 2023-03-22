116896aa820ca9a8f65b78f61a564fb25b46301a
KsenosNick <xenos-nick@mail.ru>
2023-03-22 18:25:37 +0300

    find и findIndex

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
 
-const arr = [1, 4, 4, 10];
+const arr = [2, 4, 4, 10, 20];
 
-const avg = arr.reduce((acc, value, i) => {
-  if (i != arr.length - 1) {
-    return acc + value;
-  } else {
-    return (acc + value) / arr.length;
+let elGT5;
+
+for (const el of arr) {
+  if (el > 5) {
+    elGT5 = el;
+    break;
   }
-}, 0);
+}
 
-console.log(avg);
+// console.log(elGT5);
+
+elGT5 = arr.find(el => el >5);
+const elGT5Index = arr.findIndex(el => el < 0);
+
+console.log(elGT5Index);
+
