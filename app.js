2edcdf13df945b0ed695c3f8fc301571c114f029
KsenosNick <xenos-nick@mail.ru>
2023-03-25 10:59:35 +0300

    Преобразование строки

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 'use strict';
 
-const fullUserName = 'Вася aka Terminator Perdinator Пупкин';
+let str = 'Вася Пупкина';
 
-const userName = [
-  fullUserName.split(' ').splice(0, 1),
-  userName.split(' ').splice(-1),
-]
-  .flat()
-  .join(' ');
-console.log(userNameCroped);
+console.log(str.includes('а'));
+console.log(str.startsWith('В'));
+console.log(str.endsWith('н'));
+
+console.log(new String('Вася Пупкин').includes('а'));
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('В', 'Ф'));
+console.log(str);
+
+str = 'Вася Пупкина';
+
+console.log(str.replace('а', 'и'));
+console.log(str.replaceAll('а', 'и'));
+
+console.log(str.replace(/а/, 'и'));
+console.log(str.replace(/а/g, 'и'));
+
+str = ' Вася Пупкин    \n'
+
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
