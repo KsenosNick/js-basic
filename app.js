33fffa0cee1bea4417c14ed53d5b3ce180c12ae0
KsenosNick <xenos-nick@mail.ru>
2023-03-25 18:30:49 +0300

    Упражнение - проверка номера

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 'use strict';
 
-let str = 'Вася Пупкина';
+const num1 = '9103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '  +7(910) 323-53-56';
 
-console.log(str.includes('а'));
-console.log(str.startsWith('В'));
-console.log(str.endsWith('н'));
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
-console.log(new String('Вася Пупкин').includes('а'));
+function isNumber(number) {
+  number = number.trim();
+  if (number.startsWith('+7')) {
+    number = number.replace('+7', 8);
+  }
+  number = number.replace('(', '');
+  number = number.replace(')', '');
+  number = number.replaceAll(' ', '');
+  number = number.replaceAll('-', '');
+  if (
+    (number.startsWith(89) && !isNaN(number) && number.length === 11) ||
+    (number.startsWith(9) && !isNaN(number) && number.length === 10)
+  ) {
+    console.log('Проверка успешна');
+  } else {
+    console.log('Неверный формат номера');
+  }
+}
 
-console.log(str.toLowerCase());
-console.log(str.toUpperCase());
-console.log(str.replace('В', 'Ф'));
-console.log(str);
-
-str = 'Вася Пупкина';
-
-console.log(str.replace('а', 'и'));
-console.log(str.replaceAll('а', 'и'));
-
-console.log(str.replace(/а/, 'и'));
-console.log(str.replace(/а/g, 'и'));
-
-str = ' Вася Пупкин    \n'
-
-console.log(str.trim());
-console.log(str.trimStart());
-console.log(str.trimEnd());
+isNumber(num1);
