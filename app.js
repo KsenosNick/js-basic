2c5cdc3e74b75645d5ec67def486256fdb1a869a
KsenosNick <xenos-nick@mail.ru>
2023-03-25 18:34:39 +0300

    Строки и массивы

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
 'use strict';
 
-const num1 = '9103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = '  +7(910) 323-53-56';
+const userFullName = 'Вася Пупкин Васильевич';
 
-const num1Error = '89103235';
-const num2Error = '+7d910d323-53-56';
-const num3Error = '9+7103235356';
-const num4Error = '89103g35356';
+console.log(userFullName.split(' '));
+const [firstName, familyName, lastName] = userFullName.split(' ');
+console.log(firstName);
+console.log(familyName);
+console.log(lastName);
 
-function isNumber(number) {
-  number = number.trim();
-  if (number.startsWith('+7')) {
-    number = number.replace('+7', 8);
-  }
-  number = number.replace('(', '');
-  number = number.replace(')', '');
-  number = number.replaceAll(' ', '');
-  number = number.replaceAll('-', '');
-  if (
-    (number.startsWith(89) && !isNaN(number) && number.length === 11) ||
-    (number.startsWith(9) && !isNaN(number) && number.length === 10)
-  ) {
-    console.log('Проверка успешна');
-  } else {
-    console.log('Неверный формат номера');
-  }
-}
-
-isNumber(num1);
+const arr = ['Ты', 'знаешь', 'JS'];
+console.log(arr.join(' '));
