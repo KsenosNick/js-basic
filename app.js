d3c52ae19c3016046f2042b590a6c6d3d5f75b7c
KsenosNick <xenos-nick@mail.ru>
2023-03-26 11:59:23 +0300

    Обращение к элементам

--- app.js
+++ app.js
@@ -6,4 +6,24 @@ const user = {
   surname: 'Пупкин',
   age: 24,
   skills: ['Программирование', 'Готовка'],
+  eduBasic: 'Школа №3',
+  eduPro: 'МФТИ',
 };
+
+console.log(user);
+console.log(user.name);
+console.log(user.skills[0]);
+
+console.log(user['skills']);
+
+const level = 'Pro';
+console.log(user['edu' + level]);
+
+// const res = prompt('Введите свойство: ');
+// console.log(user[res]);
+
+user['city'] = 'Москва';
+console.log(user);
+
+user.age = 30;
+user['age'] = 35;
