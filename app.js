9589b879704e895acd4ee0624fe15aa7004f82d3
KsenosNick <xenos-nick@mail.ru>
2023-03-26 12:26:01 +0300

    Упражнение - преобразование объектов

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const users = [
-  { name: 'Вася', age: 30 },
-  { name: 'Катя', age: 18 },
-  { name: 'Аня', age: 25 },
-  { name: 'Петя', age: 33 },
+  {
+    name: 'Вася',
+    surname: 'Пупкин',
+    age: 30,
+    skills: ['Разработка', 'DevOps'],
+  },
+  {
+    name: 'Катя',
+    surname: 'Белова',
+    age: 18,
+    skills: ['Дизайн'],
+  },
 ];
 
-users.sort((user1, user2) => user1.age - user2.age);
-console.log(users);
+const users2 = users.map((user) => {
+  return {
+    fullName: `${user.name} ${user.surname}`,
+    skillsNum: user.skills.length,
+  };
+});
+
+console.log(users2);
