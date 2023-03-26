27bdda1b696396f9be0b38e5ee9dc5faf4dc1720
KsenosNick <xenos-nick@mail.ru>
2023-03-26 12:18:38 +0300

    Упражнение - сортировка пользователей

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 'use strict';
 
-const userArray = ['Вася', 'Пупкин', 24];
-const user = {
-  name: 'Вася',
-  surname: 'Пупкин',
-  age: 24,
-  skills: ['Программирование', 'Готовка'],
-  eduBasic: 'Школа №3',
-  eduPro: 'МФТИ',
-};
+const users = [
+  { name: 'Вася', age: 30 },
+  { name: 'Катя', age: 18 },
+  { name: 'Аня', age: 25 },
+  { name: 'Петя', age: 33 },
+];
 
-console.log(user);
-console.log(user.name);
-console.log(user.skills[0]);
-
-console.log(user['skills']);
-
-const level = 'Pro';
-console.log(user['edu' + level]);
-
-// const res = prompt('Введите свойство: ');
-// console.log(user[res]);
-
-user['city'] = 'Москва';
-console.log(user);
-
-user.age = 30;
-user['age'] = 35;
+users.sort((user1, user2) => user1.age - user2.age);
+console.log(users);
