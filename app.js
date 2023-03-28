1b13d2b4db3c189ff7e5268e065e38a8eb277d65
KsenosNick <xenos-nick@mail.ru>
2023-03-28 19:26:29 +0300

    Упражнение - склад

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 'use strict';
 
-const cities = {
-  msk: {
-    temp: {
-      celsius: 25,
-    },
+const warehouse = {
+  goods: [],
+  findGoodById: function (id) {
+    return this.goods.find((good) => good.id === id);
   },
-  spb: {},
+  addGood: function (good) {
+    const existedGood = this.findGoodById(good.id);
+    if (existedGood) {
+      console.log(`Товар id = ${good.id} уже есть`);
+      return;
+    }
+    this.goods.push(good);
+  },
+  getWeightKg: function () {
+    return this.goods.reduce((acc, good) => {
+      return (acc += good.weight?.kg ? good.weight.kg : 0);
+    }, 0);
+  },
 };
 
-const city = 'spb';
+/* Товары */
 
-if (cities[city] != undefined && cities[city].temp != undefined) {
-  console.log(cities[city].temp.celsius);
-}
+const car = {
+  id: 1,
+  weight: {
+    kg: 1000,
+  },
+  brand: 'Ford',
+};
 
-console.log(cities[city]?.temp?.celsius);
+const chair = {
+  id: 2,
+  weight: {
+    kg: 2,
+  },
+};
+
+const paper = {
+  id: 3,
+  color: 'red',
+};
+
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+warehouse.addGood(paper);
+
+console.log(warehouse);
+
+// console.log(warehouse.findGoodById(1));
+console.log(warehouse.getWeightKg());
+// console.log();
