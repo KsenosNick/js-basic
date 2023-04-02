ccb5efafdb82b3b35427a90bc42560d7a48820b9
KsenosNick <xenos-nick@mail.ru>
2023-04-02 19:37:27 +0300

    Arguments

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 'use strict';
 
-const user = {
-  firstName: 'Вася',
-  lastName: 'Пупкин',
-  age: 20,
-  getUserInfo: function () {
-    console.log(this);
-    console.log(`${this.firstName} ${this.lastName}`);
+function sumNum(num1, num2) {
+  console.log(this);
+  console.log(arguments);
+  return num1 + num2;
+}
 
-    const canDrink = () => {
-      console.log(this);
-      if (this.age >= 18) {
-        console.log('Может уже пить');
-      } else {
-        ('Не может пить!');
-      }
-    };
-    canDrink();
-  },
-  getUserInfoArrow: () => {
-    console.log(this);
-    console.log(`${this.firstName} ${this.lastName}`);
-  },
+console.log(sumNum(1, 3, 4, 5));
+
+const sumNum = (num1, num2) => {
+  console.log(this);
+  console.log(arguments);
+  return num1 + num2;
 };
-
-user.getUserInfo();
-user.getUserInfoArrow();
