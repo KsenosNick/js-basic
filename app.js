508ac521afc263466312411007240a6dd570b244
KsenosNick <xenos-nick@mail.ru>
2023-04-04 20:21:43 +0300

    Упражнение - управление this

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 'use strict';
 
-const audi = {
-  make: 'Audi',
-  model: 'A3',
-  year: 2021,
-  damages: [],
+const user = {
+  password: '1234',
 };
 
-const carManipulations = {
-  addDamage(part, rate) {
-    this.damages.push({ part, rate });
-    console.log(`Добавить повреждение на ${this.make} ${this.model}`);
-  },
-};
+function removePassword(reset) {
+  if (reset) {
+    this.password = undefined;
+  } else {
+    this.password = '1';
+  }
+}
 
-const addDamageAudi = carManipulations.addDamage.bind(audi);
-addDamageAudi('Крыло', 3);
-console.log(audi);
+const resetUserPassword = removePassword.bind(user, true);
 
-const addDamageAudiRoof = carManipulations.addDamage.bind(audi, 'Крыша');
-addDamageAudiRoof(5);
-addDamageAudiRoof(3);
-console.log(audi);
+resetUserPassword();
+console.log(user);
