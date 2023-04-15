c46a236b56a11a439c5f3c0a8a65f646463ee63a
KsenosNick <xenos-nick@mail.ru>
2023-04-15 20:14:31 +0300

    Установка атрибутов

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ function submitForm() {
   document.querySelector('.panel').innerText = input;
   document.querySelector('input').value = '';
   // document.querySelector('.notification').classList.add('notification_active');
-  document
-    .querySelector('.notification')
-    .classList.remove('notification_hidden');
-  return input;
+  // document
+  //   .querySelector('.notification')
+  //   .classList.remove('notification_hidden');
+  // return input;
+  console.log(document.querySelector('.notification').getAttribute('class'));
+  // document.querySelector('.notification').setAttribute('class', 'notification'); - НЕ РЕКОМЕНДУЕТСЯ
+  document.querySelector('.notification').setAttribute('user-id', 1);
+  console.log(
+    Number(document.querySelector('.notification').getAttribute('user-id'))
+  );
 }
 
 function inputChanged(e) {
