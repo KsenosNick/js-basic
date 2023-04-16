2ad2831effd76214e8cb4a5209972164c800b5c3
KsenosNick <xenos-nick@mail.ru>
2023-04-16 10:32:57 +0300

    Добавление html на лету

--- app.js
+++ app.js
@@ -25,3 +25,13 @@ function inputChanged(e) {
     submitForm();
   }
 }
+
+const panelText = 'Панель';
+const panelClass = 'button';
+const newElement = document.createElement('div');
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+// newElement.innerText = 'Кнопка';
+newElement.innerHTML = `<button class=${panelClass}>${panelText}</button>`;
+
+document.querySelector('.test').appendChild(newElement);
