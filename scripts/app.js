1a65d6ce558000b8f2448a0d3fa019f6f0260c6c
KsenosNick <xenos-nick@mail.ru>
2023-04-23 18:04:18 +0300

    Рендер шапки

--- scripts/app.js
+++ scripts/app.js
@@ -5,33 +5,38 @@ const HABBIT_KEY = 'HABBIT_KEY';
 
 /* page */
 const page = {
-	menu: document.querySelector('.menu__list')
-}
+  menu: document.querySelector('.menu__list'),
+  header: {
+    h1: document.querySelector('.h1'),
+    progressPercent: document.querySelector('.progress__percent'),
+    progressCoverBar: document.querySelector('.progress__cover-bar'),
+  },
+};
 
 /* utils */
 function loadData() {
-	const habbitsString = localStorage.getItem(HABBIT_KEY);
-	const habbitArray = JSON.parse(habbitsString);
-	if (Array.isArray(habbitArray)) {
-		habbits = habbitArray;
-	}
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
 }
 
 function saveData() {
-	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
 /* render */
 function rerenderMenu(activeHabbit) {
   if (!activeHabbit) {
-		return;
-	}
+    return;
+  }
   for (const habbit of habbits) {
     const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
     if (!existed) {
       const element = document.createElement('button');
       element.setAttribute('menu-habbit-id', habbit.id);
-			element.classList.add('menu__item');
+      element.classList.add('menu__item');
       element.addEventListener('click', () => rerender(habbit.id));
       element.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="${habbit.name}" />`;
       if (activeHabbit.id === habbit.id) {
@@ -48,9 +53,23 @@ function rerenderMenu(activeHabbit) {
   }
 }
 
+function rerenderHead(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+  page.header.h1.innerText = activeHabbit.name;
+  const progress =
+    activeHabbit.days.length / activeHabbit.target > 1
+      ? 100
+      : (activeHabbit.days.length / activeHabbit.target) * 100;
+  page.header.progressPercent.innerText = progress.toFixed(0);
+  page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`)
+}
+
 function rerender(activeHabbitId) {
   const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
   rerenderMenu(activeHabbit);
+  rerenderHead(activeHabbit);
 }
 
 /* init */
