ef0ceacbd110e41affba2f76e46c731ce1deda98
KsenosNick <xenos-nick@mail.ru>
2023-05-08 11:50:01 +0300

    Добавление и удаление дней

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 /* page */
 const page = {
@@ -73,7 +74,7 @@ function rerenderContent(activeHabbit) {
       <div class="habbit__comment">
         ${activeHabbit.days[index].comment}
       </div>
-      <button class="habbit__delete">
+      <button class="habbit__delete" onclick="deleteDay(${index})">
         <img src="./images/delete.svg" alt="Удалить день ${
           +index + 1
         }" srcset="" />
@@ -84,6 +85,7 @@ function rerenderContent(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
+  globalActiveHabbitId = activeHabbitId;
   const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
   if (!activeHabbit) {
     return;
@@ -95,10 +97,36 @@ function rerender(activeHabbitId) {
 
 /* work with days */
 function addDays(event) {
+  const form = event.target;
   event.preventDefault();
-  console.log(event);
-  const data = new FormData(event.target);
-  console.log(data.get('comment'));
+  const data = new FormData(form);
+  const comment = data.get('comment');
+  form['comment'].classList.remove('error');
+  if (!comment) {
+    form['comment'].classList.add('error');
+  }
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalActiveHabbitId) {
+      return {
+        ...habbit,
+        days: habbit.days.concat([{ comment }]),
+      };
+    }
+    return habbit;
+  });
+  form['comment'].value = '';
+  rerender(globalActiveHabbitId);
+  saveData();
+}
+
+function deleteDay(index) {
+  habbits.map((habbit) => {
+    if (habbit.id === globalActiveHabbitId) {
+      habbit.days.splice(index, 1);
+    }
+  });
+  rerender(globalActiveHabbitId);
+  saveData();
 }
 
 /* init */
